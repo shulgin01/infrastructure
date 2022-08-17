c15f4a34c90f694b187d4135f625bdad01743530
shulgin01 <shulgin01@inbox.ru>
2022-08-17 16:49:59 +0300

    test tags

--- release.js
+++ release.js
@@ -20,7 +20,7 @@ const getCommits = async (tag) => {
 }
 
 const getTags = async () => {
-    return (await execute('git', ['tag'])).split("\n").filter(Boolean);
+    return (await execute('git', ['tag'])).split("\n");
 }
 
 const execute = async (command, args) => {
