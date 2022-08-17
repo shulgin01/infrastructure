4eda56e2e4559211642e5e2448d1472c2c25d6e5
shulgin01 <shulgin01@inbox.ru>
2022-08-17 16:05:40 +0300

    test commits

--- release.js
+++ release.js
@@ -1,9 +1,47 @@
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
-console.log('RELEASE')
 const release = async () => {
-    let currentTag = github.context.payload.ref
-    console.info(currentTag)
+    const currentTag = getTag()
+    console.log(currentTag)
+    const commits = await getCommits('rc-0.0.1')
+    console.log(commits)
 }
 
-release().then(() => "Success")
+const getTag = () => github.context.payload.ref.replace("refs/tags/", "") ?? ""
+
+const getCommits = async (tag) => {
+    const tags = await getTags()
+    const currentIndex = tags.indexOf(tag)
+    const commitsFilter = tags.length === 1 ? tag : `${tag}...${tags[currentIndex - 1]}`;
+    const releaseCommits = await execute('git', ['log', '--pretty=format:"%H %an %s"', commitsFilter]);
+    return releaseCommits.replace(/"/g, "");
+}
+
+const getTags = async () => {
+    return (await execute('git', ['tag'])).split('\n').filter(Boolean)
+}
+
+const execute = async (command, args) => {
+    let output = ""
+    let error = ""
+
+    const options = {};
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        },
+        stderr: (data) => {
+            error += data.toString();
+        }
+    };
+
+    await exec.exec(command, args, options)
+
+    if (error){
+        throw new Error(`Unable to execute ${command}`)
+    }
+    return output
+}
+
+release().then(() => console.log("Success"))
